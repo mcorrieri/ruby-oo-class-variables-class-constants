962b4f5aff0d5635fc33c128db0b15da981ec25c
Michael <Mlcorrieri@gmail.com>
2021-04-28 18:15:07 -0400

    commit tree b19e013fc327f6c1844bbe692e1b457dbf00878e

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand, :brands
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+
   end
 
   def cobble
@@ -11,4 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brands=(brands)
+    @brands = brands
+    BRANDS << brands
+  end
 end
